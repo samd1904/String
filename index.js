eaa80a92b579c9b546d8dd01ff3679adca01c456
Saumya Darne <samd1904@gmail.com>
2024-09-03 01:25:49 +0530

    Base chat socket working

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-
+//const { Server } = require('socket.io');
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:5173", // or the URL of your Vue.js app
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get('/', (req, res)=>{res.sendFile(__dirname+"/index.html")});
 
 io.on('connection', socket=>{
-    console.log('A user has connected');
-    socket.on('chat message', msg=>{
-         io.emit('chat message', msg);
-    })
+  console.log('A user has connected');
+  socket.on('chat message', msg=>{
+    console.log("chat recieved", msg)
+    io.emit('chat message', msg);
+  })
 })
 
 server.listen(3000, ()=>{
-    console.log("Listening on port 3000....");
-})
+  console.log("Listening on port 3000....");
+});
