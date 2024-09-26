5cfe6f188ee78841fef3e4bcd4890ee57b121755
Saumya Darne <samd1904@gmail.com>
2024-09-26 20:01:19 +0530

    from feature added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 //const { Server } = require('socket.io');
 const io = require('socket.io')(server, {
   cors: {
-    origin: ["http://localhost:5173","https://cool-banoffee-1bc218.netlify.app"], // or the URL of your Vue.js app
+    origin: ["http://localhost:5173", "https://cool-banoffee-1bc218.netlify.app"], // or the URL of your Vue.js app
     methods: ["GET", "POST"]
   }
 });
@@ -23,14 +23,11 @@ io.use((socket, next) => {
 });
 
 io.on('connection', socket => {
-  console.log('A user has connected');
-  console.log('socket room', socket.roomId);
   socket.join(socket.roomId)
-  socket.emit("room connected", socket.roomId)
+  socket.emit("room connected",{ roomId: socket.roomId, username: socket.username })
   socket.on('chat message', (data) => {
-    console.log('recieved chat 2')
     console.log(data)
-    io.to(data.to).emit('chat message', data.msg);
+    io.to(data.to).emit('chat message',{msg: data.msg, from: data.from});
   })
 })
 
