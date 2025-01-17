36f111ed9497dfc9ec1bab5ef7f950ae4036442f
Saumya Darne <samd1904@gmail.com>
2025-01-17 16:48:23 +0530

    code refactor

--- server.js
+++ server.js
@@ -1,38 +1,12 @@
 import "dotenv/config.js";
 import express from 'express';
-const app = express();
 import http from 'http';
+const app = express();
 const server = http.createServer(app);
 import { initializeSocket } from './socket.js';
 import './database.js';
 initializeSocket(server);
-//const { Server } = require('socket.io');
-//const io = require('socket.io')(server, {
-//  cors: {
-//    origin: ["http://localhost:5173", "https://cool-banoffee-1bc218.netlify.app"], // or the URL of your Vue.js app
-//    methods: ["GET", "POST"]
-//  }
-//});
 app.get('/', (req, res) => { res.sendFile(__dirname + "/index.html") });
-
-//io.use((socket, next) => {
-//  const { username, roomId } = socket.handshake.auth;
-//  if (!username) {
-//    return next(new Error("invalid username"));
-//  }
-//  socket.username = username;
-//  socket.roomId = roomId;
-//  next();
-//});
-//io.on('connection', socket => {
-//  socket.join(socket.roomId)
-//  socket.emit("room connected",{ roomId: socket.roomId, username: socket.username })
-//  socket.on('chat message', (data) => {
-//    console.log(data)
-//    io.to(data.to).emit('chat message',{msg: data.msg, from: data.from});
-//  })
-//})
-//
 const PORT = process.env.PORT || 4080;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
